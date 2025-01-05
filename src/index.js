1bd03bfb768261ed80690d741c56387d5efd173e
Shashank924 <shashankshekhar955@gmail.com>
2025-01-05 16:32:05 +0530

    Added City Model and Migrations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
 
+const CityRepository = require('./repository/cityRepository');
+
 const setupAndStartServer = async () => {
 
     // create the express object
     const app = express();
 
-    app.listen(PORT , () => {
+    app.listen(PORT , async () => {
         console.log(`server started at port ${PORT}`);
+
+        const cityObj = new CityRepository();
+        cityObj.createCity({name : "Patna"});
     })
 }
 
